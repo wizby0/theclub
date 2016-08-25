507aae7f5687c65c70202f8993ce2d98dfd7e011
wizby0 <wizby0@naver.com>
2016-08-25 22:22:29 +0900

    map information added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,10 @@ class WelcomeController < ApplicationController
 		@hash = Gmaps4rails.build_markers(@locations) do |loca, marker|
 			marker.lat loca.latitude
 			marker.lng loca.longitude
+		    marker.infowindow render_to_string(
+		      :partial => "/welcome/location_info", 
+		      :locals => {location: loca}
+		    )
 		end
 	end
 
